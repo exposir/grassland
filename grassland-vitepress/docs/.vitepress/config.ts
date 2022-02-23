8bd935d05965fcbe252d99dfbdbe4da24ebb1d5a
menglingyu <menglingyu@taou.com>
2022-02-23 21:22:21 +0800

    Update config.ts

--- grassland-vitepress/docs/.vitepress/config.ts
+++ grassland-vitepress/docs/.vitepress/config.ts
@@ -1,39 +1,10 @@
-const sidebar = {
-  home: [
-    ["/", "Enjoy Life"],
-    ["//互联网常用名词", "互联网常用名词"],
-    ["/pin/有趣的题目", "有趣的题目"],
-    ["/pin/Leetcode Top 100", "Leetcode Top 100"],
-  ],
-};
 module.exports = {
   title: "Exposir",
   description: "孟世博的博客",
   dest: "public",
   serviceWorker: false,
-  head: [
-    [
-      "script",
-      {},
-      `
-      var _hmt = _hmt || [];
-      (function() {
-        var hm = document.createElement("script");
-        hm.src = "https://hm.baidu.com/hm.js?5343da773d52d4e68e0bcaf4c1654722";
-        var s = document.getElementsByTagName("script")[0]; 
-        s.parentNode.insertBefore(hm, s);
-      })();
-    `,
-    ],
-  ],
-  configureWebpack: {
-    resolve: {
-      alias: {},
-    },
-  },
-  markdown: {},
   themeConfig: {
-    repo: "exposir/issues",
+    repo: "exposir/grassland",
     repoLabel: "Github",
     editLinks: true,
     docsDir: "docs",
