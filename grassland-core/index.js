f1cbdce58c589e7d1a3c49d057c51e39f3b390ed
menglingyu <menglingyu@taou.com>
2022-02-23 21:25:50 +0800

    updatef

--- grassland-core/index.js
+++ grassland-core/index.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const fs = require("fs");
 
 axios(
-  "https://api.github.com/repos/exposir/issue/issues?state=all&per_page=100"
+  "https://api.github.com/repos/exposir/issue/grassland?state=all&per_page=100"
 ).then((res) => {
   console.log(res);
   res.data.forEach((item) => {
