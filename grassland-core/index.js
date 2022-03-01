171cc36f2f339c9a1b3720a6822bce8451b538e6
menglingyu <menglingyu@taou.com>
2022-03-01 15:15:48 +0800

    Update index.js

--- grassland-core/index.js
+++ grassland-core/index.js
@@ -1,11 +1,5 @@
 const fs = require("fs");
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
-// const axios = require("axios");
-const axios = __importDefault(require("axios"));
+var axios = require("axios");
 const base = "./docs/";
 const sidebar = [];
 
